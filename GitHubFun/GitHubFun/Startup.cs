f54a7322c554f4684bc026fa7109a9c350add6e1
Brian Akers <bsakers12345@gmail.com>
2018-06-14 09:22:45 -0400

    Added comment in Startup.cs

--- GitHubFun/GitHubFun/Startup.cs
+++ GitHubFun/GitHubFun/Startup.cs
@@ -25,7 +25,7 @@ namespace GitHubFun
         {
             services.AddMvc();
         }
-
+        //comment for commit
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
